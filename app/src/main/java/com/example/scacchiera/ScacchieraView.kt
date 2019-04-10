b8376e0054bd62e4c51fc7ea0773621877d9f65f
luigi <louis.1995.11.04@gmail.com>
2019-04-10 22:11:25 +0200

    La matrice di Rect viene adesso disegnata in base ad una matrice di Boolean. Aggiunta il cambiamento di colore di riga e colonna in caso di touch

--- app/src/main/java/com/example/scacchiera/ScacchieraView.kt
+++ app/src/main/java/com/example/scacchiera/ScacchieraView.kt
@@ -4,21 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 class ScacchieraView : View {
     var dim = 4
-    private val bRectMatrix = ArrayList<Boolean>().also {
-        var b=true
-        for (i in 0 until (dim*dim)){
-            it[i]=b
-            b=!b
-        }
-    }
-    private val paint1=Paint().also { it.color=Color.BLUE }
-    private val paint2=Paint().also { it.color=Color.YELLOW }
+    private var pad = 5
+    private var bRectMatrix = generateMatrix()
+
+
+    private val paint1 = Paint().also { it.color = Color.BLUE }
+    private val paint2 = Paint().also { it.color = Color.YELLOW }
+
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
@@ -26,17 +26,26 @@ class ScacchieraView : View {
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val rectHeight = this.height / dim
-        val rectWidth = this.width / dim
-
-
-        for(i in 0 until dim){
-            for(j in 0 until dim){
-                if(bRectMatrix[i*dim+j]){
-                    canvas!!.drawRect((i*rectWidth).toFloat(),(j*rectHeight).toFloat(),((i+1)*rectWidth).toFloat(),((j+1)*rectHeight).toFloat(),paint1)
-                }else{
-                    canvas!!.drawRect((i*rectWidth).toFloat(),(j*rectHeight).toFloat(),((i+1)*rectWidth).toFloat(),((j+1)*rectHeight).toFloat(),paint2)
-
+        val rectHeight = height / dim
+        val rectWidth = width / dim
+        for (i in 0 until dim) {
+            for (j in 0 until dim) {
+                if (bRectMatrix[i * dim + j]) {
+                    canvas!!.drawRect(
+                        (i * rectWidth).toFloat() + pad,
+                        (j * rectHeight).toFloat() + pad,
+                        ((i + 1) * rectWidth).toFloat() - pad,
+                        ((j + 1) * rectHeight).toFloat() - pad,
+                        paint1
+                    )
+                } else {
+                    canvas!!.drawRect(
+                        (i * rectWidth).toFloat() + pad,
+                        (j * rectHeight).toFloat() + pad,
+                        ((i + 1) * rectWidth).toFloat() - pad,
+                        ((j + 1) * rectHeight).toFloat() - pad,
+                        paint2
+                    )
                 }
             }
         }
@@ -44,15 +53,38 @@ class ScacchieraView : View {
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
-        if ( event?.action == MotionEvent.ACTION_UP){
-            val riga = (event.y/dim).toInt()
-            val col=(event.x/dim).toInt()
+        val rectHeight = height / dim
+        val rectWidth = width / dim
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+
+            val riga = (event.y / rectHeight).toInt() + 1
+            val col = (event.x / rectWidth).toInt() + 1
 
 
+            bRectMatrix.apply {
+                this.forEachIndexed { index, b ->
+                    if (index % dim == riga-1 || (((col-1)*dim) until (col-1)*dim+dim).contains(index)) {
+                        this[index] = !b
+                    }
+                }
+            }
 
+            invalidate()
         }
 
         return super.onTouchEvent(event)
+    }
+
+    fun generateMatrix():MutableList<Boolean>{
+        return mutableListOf<Boolean>().also {
+            var b = true
+            for (i in 1..(dim * dim)) {
+                it.add(b)
+                if (i % dim != 0) {
+                    b = !b
+                }
+            }
+        }
     }
 }
 
