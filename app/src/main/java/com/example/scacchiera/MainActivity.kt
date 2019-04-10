1c350dd7e3f7259a5f5cfb947833cbd9083c1325
luigi <louis.1995.11.04@gmail.com>
2019-04-10 23:35:39 +0200

    La SeekBar adesso permette di cambiare la dimensione della scacchiera. Le vittorie e il numero di mosse sono mostrati correttamente. Aggiornato il pulsante Gioca

--- app/src/main/java/com/example/scacchiera/MainActivity.kt
+++ app/src/main/java/com/example/scacchiera/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.scacchiera
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dimSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                dimTextView.text = " ${i + 4}"
+                scacchiera.dim = i + 4
+                scacchiera.isPlaying = false
+                scacchiera.regenerateMatrix()
+                scacchiera.invalidate()
+            }
+        })
+
+        scacchiera.setOnVictoryListener {
+            winText.text = " $it"
+        }
+
+        scacchiera.setOnMoveListener {
+            numClickText.text = " $it"
+        }
+
+        playButton.setOnClickListener {
+            scacchiera.isPlaying = true
+        }
     }
 }
--- app/src/main/java/com/example/scacchiera/ScacchieraView.kt
+++ app/src/main/java/com/example/scacchiera/ScacchieraView.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Rect
+import android.support.design.widget.Snackbar
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 class ScacchieraView : View {
     var dim = 4
+    var isPlaying = false
     private var pad = 5
     private var bRectMatrix = generateMatrix()
+    private var victories = 0
+    private var moves = 0
 
+    private lateinit var moveListener: onMoveListener
+    private lateinit var victoryListener: onVictoryListener
 
     private val paint1 = Paint().also { it.color = Color.BLUE }
     private val paint2 = Paint().also { it.color = Color.YELLOW }
@@ -22,7 +26,6 @@ class ScacchieraView : View {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -56,35 +59,82 @@ class ScacchieraView : View {
         val rectHeight = height / dim
         val rectWidth = width / dim
         if (event?.action == MotionEvent.ACTION_DOWN) {
+            if (isPlaying) {
+                val riga = (event.y / rectHeight).toInt() + 1
+                val col = (event.x / rectWidth).toInt() + 1
 
-            val riga = (event.y / rectHeight).toInt() + 1
-            val col = (event.x / rectWidth).toInt() + 1
 
-
-            bRectMatrix.apply {
-                this.forEachIndexed { index, b ->
-                    if (index % dim == riga-1 || (((col-1)*dim) until (col-1)*dim+dim).contains(index)) {
-                        this[index] = !b
+                bRectMatrix.apply {
+                    this.forEachIndexed { index, b ->
+                        if (index % dim == riga - 1 || (((col - 1) * dim) until (col - 1) * dim + dim).contains(index)) {
+                            this[index] = !b
+                        }
                     }
                 }
-            }
 
-            invalidate()
+                invalidate()
+
+                if (hasWon()) {
+                    isPlaying = false
+                    Snackbar.make(this, "Complimenti!! Hai vinto|", Snackbar.LENGTH_LONG).show()
+                    victoryListener.onVictory(victories)
+                }
+
+                moveListener.onMove(++moves)
+            } else {
+                Snackbar.make(this, "Premi Gioca per cominciare a giocare.", Snackbar.LENGTH_LONG).show()
+            }
         }
 
         return super.onTouchEvent(event)
     }
 
-    fun generateMatrix():MutableList<Boolean>{
+    private fun generateMatrix(): MutableList<Boolean> {
         return mutableListOf<Boolean>().also {
             var b = true
             for (i in 1..(dim * dim)) {
                 it.add(b)
-                if (i % dim != 0) {
+                if (i % dim != 0 || dim%2!=0) {
                     b = !b
                 }
             }
         }
+    }
+
+    fun regenerateMatrix() {
+        bRectMatrix = generateMatrix()
+        moves=0
+        moveListener.onMove(0)
+    }
+
+    private fun hasWon(): Boolean {
+        return !bRectMatrix.contains(false)
+    }
+
+
+    fun setOnVictoryListener(listener: (n: Int) -> Unit) {
+        victoryListener = object : onVictoryListener {
+            override fun onVictory(n: Int) {
+                listener(n)
+            }
+        }
+    }
+
+    fun setOnMoveListener(listener: (n: Int) -> Unit) {
+        moveListener = object : onMoveListener {
+            override fun onMove(n: Int) {
+                listener(n)
+            }
+        }
+    }
+
+
+    interface onVictoryListener {
+        fun onVictory(n: Int)
+    }
+
+    interface onMoveListener {
+        fun onMove(n: Int)
     }
 }
 
